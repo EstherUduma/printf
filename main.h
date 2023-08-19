734d38735b1c36bd9dc479781da5018f1dcfefcc
Esther Uduma <estheruduma23@gmail.com>
2023-08-18 21:46:43 -0700

    update

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header files */
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+* struct CharPrint - Handles printing task
+* @c: format specifier
+* @pr_Fntn: function pointer
+*/
+typedef struct CharPrint
+{
+	char *c;
+	int (*pr_Fntn)(va_list arg);
+} PrintInstruction;
+
+/* Prototypes */
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
